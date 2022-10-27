5c815a4850e363153889416eaa9d47336457b04d
sjdahfweh <114200773+sjdahfweh@users.noreply.github.com>
2022-10-26 22:32:28 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -4,21 +4,25 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    String fruit = null;
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+            return String.format("fruit %s", fruit);
         } else {
             System.out.println("Path: " + url.getPath());
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    fruit = (String)parameters[1];
+                    return String.format("the fruit is now %s",fruit);
+                }
+            }
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    fruit = (String)parameters[1];
+                    return String.format("the fruit is now %s",fruit);
                 }
             }
             return "404 Not Found!";
